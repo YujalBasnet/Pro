369f7ee37b77ac0cfdf0768db4b83b7a30d0afbe
Yujal <laxmikhulalbasnet@gmail.com>
2026-05-05 19:24:40 +0545

    Dlt

--- mechanic-hiring-frontend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-
-const login = (data) => api.post("/auth/login", data);
-const register = (data) => api.post("/auth/register", data);
-const getCurrentUser = () => api.get("/auth/me");
-const logout = () => localStorage.removeItem("token");
-
-export default { login, register, getCurrentUser, logout };
