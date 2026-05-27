8d2642370db31795a1e6336a47e18116ced0852b
Yujal <laxmikhulalbasnet@gmail.com>
2026-05-27 11:49:24 +0545

    !!

--- Std_Mngmt/src/main/java/com/stdmngmt/App.java
+++ Std_Mngmt/src/main/java/com/stdmngmt/App.java
@@ -1,17 +1,32 @@
 package com.stdmngmt;
 
+import com.stdmngmt.repository.CourseRepository;
+import com.stdmngmt.repository.EnrollmentRepository;
 import com.stdmngmt.repository.StudentRepository;
+import com.stdmngmt.repository.inmemory.InMemoryCourseRepository;
+import com.stdmngmt.repository.inmemory.InMemoryEnrollmentRepository;
 import com.stdmngmt.repository.inmemory.InMemoryStudentRepository;
+import com.stdmngmt.service.CourseService;
+import com.stdmngmt.service.EnrollmentService;
 import com.stdmngmt.service.StudentService;
 import com.stdmngmt.ui.ConsoleMenu;
 import com.stdmngmt.ui.InputReader;
 
 public class App {
     public static void main(String[] args) {
-        StudentRepository repository = new InMemoryStudentRepository();
-        StudentService service = new StudentService(repository);
+        StudentRepository studentRepository = new InMemoryStudentRepository();
+        CourseRepository courseRepository = new InMemoryCourseRepository();
+        EnrollmentRepository enrollmentRepository = new InMemoryEnrollmentRepository();
+
+        StudentService studentService = new StudentService(studentRepository);
+        CourseService courseService = new CourseService(courseRepository);
+        EnrollmentService enrollmentService = new EnrollmentService(
+            studentRepository,
+            courseRepository,
+            enrollmentRepository
+        );
         InputReader inputReader = new InputReader();
-        ConsoleMenu menu = new ConsoleMenu(inputReader, service);
+        ConsoleMenu menu = new ConsoleMenu(inputReader, studentService, courseService, enrollmentService);
         menu.run();
     }
 }
